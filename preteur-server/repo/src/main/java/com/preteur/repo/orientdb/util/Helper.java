3bd105dad1fa4a92f51b52ced9ab949cb2dc3e49
Ashwin Ravindran <aravindran@cleo.com>
2016-06-21 17:11:26 -0500

    removed unused imports

--- preteur-server/repo/src/main/java/com/preteur/repo/orientdb/util/Helper.java
+++ preteur-server/repo/src/main/java/com/preteur/repo/orientdb/util/Helper.java
@@ -2,10 +2,6 @@ package com.preteur.repo.orientdb.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.preteur.repo.orientdb.api.impl.Preteur;
-import com.preteur.repo.orientdb.dto.UserDto;
-import com.preteur.repo.orientdb.model.Relations;
-import com.preteur.repo.orientdb.model.User;
 
 import java.util.Map;
 
